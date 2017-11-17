56c81140dcdb57837fa47cc3e40ab5099ae41411
losiak9 <33657635+losiak9@users.noreply.github.com>
2017-11-17 15:37:48 +0200

    Update server.r

--- server.r
+++ server.r
@@ -1,51 +1,42 @@
-as.numeric.factor <- function(x) {as.numeric(levels(x))[x]}
-
 read.data <- function(){
-  data <- read.table("database.txt", sep = ";", header = TRUE, encoding = "UTF-8")
-  for(col in 2:ncol(data)){
-    data[, col] <- as.numeric.factor(data[, col])
-  }
-  return (data)
+  read.table("data.txt", sep = ";", header = TRUE, encoding = "UTF-8", check.names=FALSE)
 }
-
 data <- read.data()
-
-colors <- c("#FF0000", "#000cff", "#00ff0c", "#1a9b9d", "#750db1", "#1dc87a", "#ffa800", "#d44d0a", "#ca2149", "#3a58f0")
-
-draw_ <- function(years, j){
+colors <- c("#750db1", "#1dc87a")
+draw_ <- function(countries, j){
   index <- 0
   
   for(i in 1:nrow(data)){
-    if(data[i, 1] == years[j]){
+    if(data[i, 1] == countries[j]){
       index <- i
     }
   }
-  
   if(index == 0){
     return()
   }
-  
   if(j == 1){
-    plot(2:ncol(data), data[index, 2:ncol(data)], type = "o", xaxt = "n", xlab = "dist", ylab = "speed", col = colors[j], ylim = c(0,10))
+    print(data[1, 1:51])
+    plot(1:51, data[index, 1:51], type = "o", xaxt = "n",xlab = "X Line", ylab = "Y Line", col = colors[j], ylim = c(0,145))
+    axis(1, at = 1:length(data), labels = colnames(data))
   } else {
-    lines(2:ncol(data), data[index, 2:ncol(data)], type = "o", xaxt = "n", xlab = "dist", ylab = "speed", col = colors[j])
+    lines(1:51, data[index, 1:51], type = "o", xaxt = "n", xlab = "X Line", ylab = "Y Line", col = colors[j])
   }
+  legend("topright", legend = countries, col = colors, lty = 1)
 }
-
-draw <- function(years){
-  if(is.null(years)){
+draw <- function(countries){
+  if(is.null(countries)){
     return()
   }
   
-  for(i in 1:length(years)){
-    draw_(years, i)
+  
+  for(i in 1:length(countries)){
+    draw_(countries, i)
   }
 }
-
 shinyServer(
   function(input, output) {
     output$plot <- renderPlot({
-      draw(input$year)
+      draw(input$country)
     })
   }
 )
