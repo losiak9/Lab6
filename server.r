0e87090ee3b22fe1a0a5ed26f86e969181968fb7
losiak9 <33657635+losiak9@users.noreply.github.com>
2017-11-17 13:54:45 +0200

    Update server.r

--- server.r
+++ server.r
@@ -1,3 +1,5 @@
+as.numeric.factor <- function(x) {as.numeric(levels(x))[x]}
+
 read.data <- function(){
   data <- read.table("database.txt", sep = ";", header = TRUE, encoding = "UTF-8")
   for(col in 2:ncol(data)){
@@ -8,13 +10,13 @@ read.data <- function(){
 
 data <- read.data()
 
-colors <- c("#1a9b9d", "#750db1", "#1dc87a")
+colors <- c("#FF0000", "#000cff", "#00ff0c", "#1a9b9d", "#750db1", "#1dc87a", "#ffa800", "#d44d0a", "#ca2149", "#3a58f0")
 
-draw_ <- function(countries, j){
+draw_ <- function(years, j){
   index <- 0
   
   for(i in 1:nrow(data)){
-    if(data[i, 1] == countries[j]){
+    if(data[i, 1] == years[j]){
       index <- i
     }
   }
@@ -22,34 +24,28 @@ draw_ <- function(countries, j){
   if(index == 0){
     return()
   }
- 
+  
   if(j == 1){
-    print(data[1, 2:51])
-    
-    plot(2:51, data[index, 2:51], type = "o", xaxt = "n",xlab = "Years", ylab = "Rate of GDP", col = colors[j], ylim = c(0,80000))
-    axis(1, at = 1:length(data), labels = colnames(data))
+    plot(2:ncol(data), data[index, 2:ncol(data)], type = "o", xaxt = "n", xlab = "dist", ylab = "speed", col = colors[j], ylim = c(0,10))
   } else {
-    lines(2:51, data[index, 2:51], type = "o", xaxt = "n", xlab = "Years", ylab = "Rate of GDP", col = colors[j])
+    lines(2:ncol(data), data[index, 2:ncol(data)], type = "o", xaxt = "n", xlab = "dist", ylab = "speed", col = colors[j])
   }
-  legend("topright", legend = countries, col = colors, lty = 1)
 }
 
-
-draw <- function(countries){
-  if(is.null(countries)){
+draw <- function(years){
+  if(is.null(years)){
     return()
   }
   
-  
-  for(i in 1:length(countries)){
-    draw_(countries, i)
+  for(i in 1:length(years)){
+    draw_(years, i)
   }
 }
 
 shinyServer(
   function(input, output) {
     output$plot <- renderPlot({
-      draw(input$country)
+      draw(input$year)
     })
   }
 )
